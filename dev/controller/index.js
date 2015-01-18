27c8a27c8e1c217f86ccc44eae5290d5544dde91
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-18 17:55:46 +0100

    fixed issue with copy message

--- dev/controller/index.js
+++ dev/controller/index.js
@@ -1,6 +1,11 @@
 (function() {
 
   var encryptor = require('file-encryptor')
+  var gui = require('nw.gui')
+
+  var win = gui.Window.get()
+
+  win.title = 'Shakesheare'
 
   angular
     .module('index', ['angularFileUpload'])
@@ -12,12 +17,24 @@
     vm.encdecoding = false
     vm.action = null
     vm.finished = false
+    vm.instruction = ''
 
     vm.restart = function() {
       vm.password = null
       vm.setup = true
       vm.encdecoding = vm.finished = false
       vm.action = null
+      vm.instruction = ''
+    }
+
+    vm.copyEmail = function() {
+
+      var clipboard = gui.Clipboard.get()
+
+      clipboard.clear()
+
+      clipboard.set(vm.instruction, 'text')
+
     }
 
     $scope.$watch(angular.bind(this, function() {
@@ -68,6 +85,7 @@
             $scope.$apply(function(){
               vm.encdecoding = false
               vm.finished = true
+              vm.instruction = 'Here your password: '+vm.password+'\n\nPlease download Shakesheare app to decrypt the file from here: http://shakesheare.github.io'
             })
 
             $scope.$apply()
