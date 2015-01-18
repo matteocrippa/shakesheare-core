a72f1e823fccbc1d6f1851e3700b49d8b43646cc
Matteo Crippa <matteo.crippa@gmail.com>
2015-01-18 14:39:43 +0100

    test mix js and node.js
    I want to give a try to have an unique file for js/angular and node

--- dev/assets/js/index.js
+++ dev/assets/js/index.js
@@ -1,15 +1,44 @@
 (function() {
+  
+  var encryptor = require('file-encryptor')
 
   angular
   .module('index', ['angularFileUpload'])
 
   function IndexController($log, $scope, $window) {
     var vm = this
+    
+    vm.setup = true
+    vm.encdecoding = false
+    vm.finished = false
 
     $scope.$watch(angular.bind(this, function() {
       return this.files
     }),function(newVal, oldVal){
       $log.log(newVal[0].path)
+      
+      if(newVal[0].path.length > 1){
+        if(!vm.password){
+          alert('You must set a password to encode/decode the file.')
+          return
+        }
+      }
+      
+      vm.setup = false
+      vm.encdecoding = true
+      
+      encryptor.encryptFile(newVal[0].path, newVal[0].path + '.shake', vm.password, {
+        algorithm: 'aes256'
+      }, function(err) {
+        console.log(err)
+        if(err){
+          alert(err)
+        }else{
+          vm.encdecoding = false
+          vm.finished = true
+        }
+      })
+      
     })
   }
 
